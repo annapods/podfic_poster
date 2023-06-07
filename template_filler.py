bdef946e82bc322a9b739ee802ae768cf77a6ecd
Annapods <annabelle.myrt@gmail.com>
2023-06-07 22:11:58 +0200

    wip fix get rid of cover artist if placeholder

--- template_filler.py
+++ template_filler.py
@@ -333,7 +333,7 @@ class Ao3Template(Template):
         """ Cover art """
         content = f'''<p align="center">{self.get_img(self._info["IA Cover Link"], width=250,
             img_alt_text=i18l("Cover art."), no_img_alt_text=i18l("Cover art welcome."))}'''
-        if self._info["Cover Artist(s)"]:
+        if self.not_placeholder_link(self._info["Cover Artist(s)"]):
             content += f'''<br>\n{i18l("Cover art by")} ''' \
                 + self.get_enum_links(self._info["Cover Artist(s)"])
         content += "</p>"
